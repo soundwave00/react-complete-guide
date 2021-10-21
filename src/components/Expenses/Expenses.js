f09fe2f3a6d85c4b0635745443b3fe584240980c
Andrea Cagnetta <andrea.cagnetta@techedgegroup.com>
2021-10-21 17:37:55 +0200

    Aggiunti Chart e ChartBar + fix vari

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 import Card from "../UI/Card";
 import "./Expenses.css";
 
@@ -23,6 +24,7 @@ const Expense = (props) => {
                     selected={filteredYear}
                     onChangeFilter={filterChangeHendler}
                 />
+                <ExpensesChart expenses={filteredExpenses}/>
                 <ExpensesList items={filteredExpenses} />
             </Card>
         </div>
