36bc1dff528f04c39bbfee98bc9a2acad8813e65
Andrea Cagnetta <andrea.cagnetta@techedgegroup.com>
2021-10-21 15:48:01 +0200

    cambiamenti vari

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,26 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
-    const [title, setTitle] = useState(props.title);  //il primo è il valore non ancora modificato, il secondo è quello modificato
-    console.log('EcpenseItem evalueted by react');
-
-    const clickHendler = () => {
-        setTitle('Updated!');
-        console.log(title);
-    };
-
     return (
-        <Card className="expense-item">
-            <ExpenseDate date={props.date} />
-            <div className="expense-item__description">
-                <h2>{title}</h2>
-                <div className="expense-item__price">{props.amount}€</div>
-            </div>
-            <button onClick={clickHendler}>Change title</button>
-        </Card>
+        <li>
+            <Card className="expense-item">
+                <ExpenseDate date={props.date} />
+                <div className="expense-item__description">
+                    <h2>{props.title}</h2>
+                    <div className="expense-item__price">{props.amount}€</div>
+                </div>
+            </Card>
+        </li>
     );
 };
 
